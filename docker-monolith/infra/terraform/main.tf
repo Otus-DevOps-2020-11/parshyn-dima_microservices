9d7b0c1a3abd72499e1b87df293d16909f57a7d0
Dmitriy Parshin <nosmok85@gmail.com>
2021-02-06 21:41:33 +0300

    Add a terraform configuration for deploying a docker image | HW16

---
+++ docker-monolith/infra/terraform/main.tf
@@ -0,0 +1,38 @@
+data "yandex_compute_image" "container-optimized-image" {
+  family    = "container-optimized-image"
+}
+
+provider "yandex" {
+  version                  = "~> 0.35.0"
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+resource "yandex_compute_instance" "app" {
+  count = var.instance_size
+  name  = "reddit-app-${count.index}"
+
+  metadata = {
+    docker-container-declaration = file("spec.yml")
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = data.yandex_compute_image.container-optimized-image.id
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+}
